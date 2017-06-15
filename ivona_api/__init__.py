490a2e502d4aa769b9f41603eb5d5e5ebf1ea912
Paweł Adamczak <pawel.ad@gmail.com>
2017-06-15 22:02:07 +0100

    Version bump

--- ivona_api/__init__.py
+++ ivona_api/__init__.py
@@ -9,7 +9,7 @@ from ivona_api.ivona_api import IvonaAPI  # noqa
 
 
 __title__ = 'ivona_api'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Pythonity'
 __license__ = 'MIT'
 __url__ = 'https://github.com/Pythonity/python-ivona-api'
